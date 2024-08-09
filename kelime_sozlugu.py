7d19e8c2e111f083974af34cc4bb82692b3d7e75
serhankara35 <adigesercem333@gmail.com>
2024-08-09 20:55:05 +0300

    kelime_sozlugu.py oluşturuldu
    ilk oluşturma aşaması, kodun kemiği.

---
+++ kelime_sozlugu.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    while True:
+        word = input("Yazdığınız kelime sözlükte bulunmuyor. Lütfen başka bir kelime deneyin: ")
+        if word in meme_dict.keys():
+            print(meme_dict[word])
+            break
